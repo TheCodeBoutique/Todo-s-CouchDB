49c6fff19c749ae5f8c530e50555f4f1aa9bccd4
Kyle Carriedo <kcarriedo@gmail.com>
2011-06-06 09:42:52 -0700

    CouchDB Sproutcore App

---
+++ apps/todos/data_sources/task.js
@@ -0,0 +1,153 @@
+// ==========================================================================
+// Project:   Todos.TaskDataSource
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals Todos */
+
+/** @class
+
+  (Document Your Data Source Here)
+
+  @extends SC.DataSource
+*/
+sc_require('models/task');
+Todos.TASKS_QUERY = SC.Query.local(Todos.Task, {orderBy: 'isDone,description'});
+
+
+Todos.TaskDataSource = SC.DataSource.extend({
+
+	//private variable..
+	_dbpath: 'todos',
+	
+	
+	//theses are helper methods that help format the URL string..
+	getServerPath: function(resourceName) {
+	     var path = '/' + this._dbpath + "//" + resourceName;
+	     return path;
+	},
+	     getServerView: function(viewName) {
+	     var path = '/' + this._dbpath + "/_design/app/_view/" + viewName;
+	     return path;
+	},
+
+
+  fetch: function(store, query) {
+	     if (query === Todos.TASKS_QUERY) {
+			//this is the HTTP request that we are using to is using the helper methods to call the URL
+	          SC.Request.getUrl(this.getServerView('allTasks')).json()
+	                         .header('Accept', 'application/json')
+	                         .notify(this, 'didFetchTasks', store, query)
+	                         .send();
+	          return YES;
+	     }
+	    return NO ; // return YES if you handled the query
+	  },
+	
+		didFetchTasks: function(response, store, query) {
+		  if(SC.ok(response)) {
+			
+		    var body = response.get('encodedBody');
+		    var couchResponse = SC.json.decode(body);
+		    var records = couchResponse.rows.getEach('value');
+		    store.loadRecords(Todos.Task, records);
+		    store.dataSourceDidFetchQuery(query);
+		 } else {
+		    store.dataSourceDidErrorQuery(query, response);
+		 }
+		 },
+
+  // ..........................................................
+  // RECORD SUPPORT
+  // 
+  
+  retrieveRecord: function(store, storeKey) {
+	      return NO ; // return YES if you handled the storeKey
+	  },
+  	processResponse: function(response) {
+		          if (SC.ok(response)) {
+		               var body = response.get('encodedBody');
+		               var couchResponse = SC.json.decode(body);
+		               var ok = couchResponse.ok;
+		               if (ok != YES) return {"error":true, "response":couchResponse};
+		                    var id = couchResponse.id;
+		                    var rev = couchResponse.rev;
+		                    return {"ok":true, "id": id, "rev": rev};
+		               } else {
+		    	               return {"error":true, "response":response};
+		               }
+		       },
+					getDocRev: function(doc) {
+					return doc._rev;
+					  },
+  	createRecord: function(store, storeKey) {
+		     if (SC.kindOf(store.recordTypeFor(storeKey), Todos.Task)) {
+		          SC.Request.postUrl(this.getServerPath('/')).json()
+		                            .header('Accept', 'application/json')
+		                            .notify(this, this.didCreateTask, store, storeKey)
+		                            .send(store.readDataHash(storeKey));
+		          return YES;
+		    } 
+		    return NO ; // return YES if you handled the storeKey
+		  },
+			didCreateTask: function(response, store, storeKey) {
+			     var couchRes = this.processResponse(response);
+			     if (couchRes.ok) {
+			          // Add _id and _rev to the local document for further server interaction.
+			          var localDoc = store.readDataHash(storeKey);
+			          localDoc._id = couchRes.id;
+			          localDoc._rev = couchRes.rev;
+			          store.dataSourceDidComplete(storeKey, localDoc, couchRes.id);
+			     } else {
+			        store.dataSourceDidError(storeKey, response);
+			     }
+			},
+  
+  		updateRecord: function(store, storeKey) {    
+			  if (SC.kindOf(store.recordTypeFor(storeKey), Todos.Task)) {
+			     var id = store.idFor(storeKey);
+			     var dataHash = store.readDataHash(storeKey);
+			     SC.Request.putUrl(this.getServerPath(id)).json()
+			                       .header('Accept', 'application/json')
+			                      .notify(this, this.didUpdateTask, store, storeKey)
+			                        .send(dataHash);
+			     return YES;
+			   }
+			   return NO;
+			},
+			didUpdateTask: function(response, store, storeKey) {
+			   var couchRes = this.processResponse(response);
+			   if (couchRes.ok) {
+			     // Update the local _rev of this document.
+			     var localDoc = store.readDataHash(storeKey);
+			     localDoc._rev = couchRes.rev;
+			     store.dataSourceDidComplete(storeKey, localDoc) ;
+			   } else {
+			     store.dataSourceDidError(storeKey);
+			   }
+			},
+			
+  
+  		destroyRecord: function(store, storeKey) {
+			    if (SC.kindOf(store.recordTypeFor(storeKey), Todos.Task)) {
+			          var id = store.idFor(storeKey);
+			          //var rev = this._docsRev[id];
+			          var dataHash = store.readDataHash(storeKey);
+			          var rev = this.getDocRev(dataHash);
+			          SC.Request.deleteUrl(this.getServerPath(id + "?rev=" + rev)).json()
+			                            .header('Accept', 'application/json')
+			                            .notify(this, this.didDeleteTask, store, storeKey)
+			                            .send();
+			          return YES;
+			     } 
+			     return NO ; // return YES if you handled the storeKey
+			  },
+				didDeleteTask: function(response, store, storeKey) {
+				     var couchRes = this.processResponse(response);
+				     if (couchRes.ok) {
+				          store.dataSourceDidDestroy(storeKey);
+				     } else {
+				          store.dataSourceDidError(response);
+				     }
+				  }
+  
+}) ;
---
+++ tmp/debug/build/static/sproutcore/ace/en/current/javascript.js
@@ -0,0 +1,451 @@
+/* >>>>>>>>>> BEGIN __sc_chance.js */
+if (typeof CHANCE_SLICES === 'undefined') var CHANCE_SLICES = [];CHANCE_SLICES = CHANCE_SLICES.concat(['__s78_306',
+'__s79_315',
+'__s74_239',
+'__s45_67',
+'__s53_105',
+'__s77_285',
+'__s77_282',
+'__s74_246',
+'__s49_91',
+'__s70_221',
+'__s49_84',
+'__s43_48',
+'__s79_322',
+'__s77_279',
+'__s75_259',
+'__s70_232',
+'__s49_82',
+'__s45_72',
+'__s68_204',
+'__s62_152',
+'__s70_218',
+'__s79_326',
+'__s77_298',
+'__s77_288',
+'__s40_8',
+'__s43_47',
+'__s40_24',
+'__s45_49',
+'__s62_170',
+'__s43_45',
+'__s77_299',
+'__s74_247',
+'__s74_241',
+'__s45_70',
+'__s68_212',
+'__s79_320',
+'__s77_296',
+'__s77_289',
+'__s68_203',
+'__s45_62',
+'__s70_216',
+'__s45_74',
+'__s40_5',
+'__s53_119',
+'__s62_160',
+'__s68_194',
+'__s77_276',
+'__s75_258',
+'__s45_61',
+'__s64_179',
+'__s64_177',
+'__s45_63',
+'__s77_286',
+'__s75_270',
+'__s66_186',
+'__s66_184',
+'__s45_57',
+'__s40_20',
+'__s53_120',
+'__s75_261',
+'__s62_164',
+'__s66_185',
+'__s53_112',
+'__s43_43',
+'__s68_196',
+'__s41_31',
+'__s53_115',
+'__s53_110',
+'__s62_165',
+'__s49_90',
+'__s62_155',
+'__s77_291',
+'__s54_128',
+'__s40_18',
+'__s62_167',
+'__s70_225',
+'__s53_117',
+'__s66_188',
+'__s77_278',
+'__s75_269',
+'__s45_75',
+'__s53_103',
+'__s36_2',
+'__s40_13',
+'__s41_25',
+'__s62_151',
+'__s78_307',
+'__s76_273',
+'__s74_254',
+'__s74_251',
+'__s59_143',
+'__s58_137',
+'__s40_21',
+'__s50_102',
+'__s64_182',
+'__s64_176',
+'__s75_267',
+'__s41_28',
+'__s50_99',
+'__s50_100',
+'__s79_323',
+'__s77_300',
+'__s74_244',
+'__s58_140',
+'__s53_108',
+'__s59_144',
+'__s70_215',
+'__s74_255',
+'__s49_83',
+'__s58_130',
+'__s78_302',
+'__s74_238',
+'__s70_231',
+'__s50_94',
+'__s40_22',
+'__s68_208',
+'__s77_292',
+'__s62_147',
+'__s64_175',
+'__s45_56',
+'__s58_131',
+'__s68_201',
+'__s70_220',
+'__s68_200',
+'__s43_46',
+'__s54_126',
+'__s70_229',
+'__s70_217',
+'__s59_145',
+'__s68_206',
+'__s62_158',
+'__s49_86',
+'__s50_97',
+'__s75_263',
+'__s40_16',
+'__s62_154',
+'__s64_180',
+'__s42_38',
+'__s79_327',
+'__s77_280',
+'__s45_65',
+'__s40_9',
+'__s64_178',
+'__s68_199',
+'__s75_266',
+'__s70_224',
+'__s41_30',
+'__s58_134',
+'__s62_163',
+'__s49_93',
+'__s41_36',
+'__s50_101',
+'__s68_205',
+'__s79_325',
+'__s77_283',
+'__s66_190',
+'__s36_1',
+'__s76_272',
+'__s68_211',
+'__s58_136',
+'__s45_66',
+'__s62_153',
+'__s36_3',
+'__s77_281',
+'__s47_77',
+'__s53_121',
+'__s42_37',
+'__s62_169',
+'__s62_168',
+'__s77_295',
+'__s58_139',
+'__s45_51',
+'__s62_148',
+'__s49_81',
+'__s40_17',
+'__s77_297',
+'__s75_265',
+'__s74_243',
+'__s68_197',
+'__s49_92',
+'__s48_78',
+'__s74_256',
+'__s53_104',
+'__s62_159',
+'__s66_187',
+'__s77_277',
+'__s50_98',
+'__s49_85',
+'__s79_310',
+'__s78_305',
+'__s59_146',
+'__s62_150',
+'__s62_157',
+'__s40_19',
+'__s79_319',
+'__s78_304',
+'__s76_274',
+'__s40_14',
+'__s53_106',
+'__s45_60',
+'__s62_174',
+'__s43_44',
+'__s72_236',
+'__s53_114',
+'__s45_76',
+'__s79_312',
+'__s40_15',
+'__s40_11',
+'__s70_214',
+'__s58_133',
+'__s78_303',
+'__s75_264',
+'__s74_253',
+'__s58_129',
+'__s62_162',
+'__s41_35',
+'__s66_191',
+'__s79_324',
+'__s40_6',
+'__s40_10',
+'__s49_88',
+'__s45_64',
+'__s64_183',
+'__s79_321',
+'__s72_234',
+'__s45_55',
+'__s54_123',
+'__s45_69',
+'__s53_113',
+'__s77_301',
+'__s41_27',
+'__s40_7',
+'__s40_23',
+'__s77_293',
+'__s75_268',
+'__s74_250',
+'__s54_127',
+'__s66_192',
+'__s48_79',
+'__s68_202',
+'__s42_41',
+'__s79_318',
+'__s77_294',
+'__s72_237',
+'__s72_235',
+'__s70_226',
+'__s45_50',
+'__s62_173',
+'__s41_34',
+'__s53_118',
+'__s74_248',
+'__s74_242',
+'__s45_71',
+'__s68_210',
+'__s45_54',
+'__s79_311',
+'__s70_223',
+'__s77_290',
+'__s76_275',
+'__s49_87',
+'__s74_249',
+'__s70_230',
+'__s70_227',
+'__s45_52',
+'__s62_171',
+'__s53_111',
+'__s36_0',
+'__s50_96',
+'__s76_271',
+'__s74_240',
+'__s67_193',
+'__s50_95',
+'__s45_73',
+'__s49_89',
+'__s58_135',
+'__s79_317',
+'__s70_222',
+'__s70_219',
+'__s42_40',
+'__s45_59',
+'__s79_314',
+'__s77_284',
+'__s42_42',
+'__s59_142',
+'__s41_32',
+'__s49_80',
+'__s58_138',
+'__s62_172',
+'__s79_309',
+'__s53_116',
+'__s42_39',
+'__s53_122',
+'__s66_189',
+'__s75_262',
+'__s40_12',
+'__s62_161',
+'__s79_308',
+'__s77_287',
+'__s75_260',
+'__s45_58',
+'__s64_181',
+'__s41_33',
+'__s68_209',
+'__s62_156',
+'__s41_26',
+'__s54_124',
+'__s68_198',
+'__s59_141',
+'__s79_316',
+'__s70_233',
+'__s36_4',
+'__s68_213',
+'__s74_257',
+'__s45_53',
+'__s62_166',
+'__s53_109',
+'__s54_125',
+'__s79_313',
+'__s68_207',
+'__s68_195',
+'__s58_132',
+'__s45_68',
+'__s41_29',
+'__s74_252',
+'__s74_245',
+'__s70_228',
+'__s62_149',
+'__s53_107']);
+
+/* >>>>>>>>>> BEGIN source/theme.js */
+
+/**
+ * @class
+ * SproutCore's Ace theme.
+ */
+SC.AceTheme = SC.BaseTheme.create({
+  name: "ace",
+  description: "A SproutCore built-in theme by Alex Iskander and contributors. Only supports browsers that implement CSS3."
+});
+
+// register the theme with SproutCore
+SC.Theme.addTheme(SC.AceTheme);
+
+// SC.ButtonView variants
+SC.AceTheme.PointLeft = SC.AceTheme.subtheme("point-left", "point-left");
+SC.AceTheme.PointRight = SC.AceTheme.subtheme("point-right", "point-right");
+SC.AceTheme.Capsule = SC.AceTheme.subtheme("capsule", "capsule");
+
+// Dark Variant
+/**
+ * @class SC.AceTheme.Dark
+ * SproutCore's Ace theme's Dark Side. Used in popovers or wherever you
+ * choose (use it by making the view or one of its parents have a
+ * themeName of 'dark').
+ */
+SC.AceTheme.Dark = SC.AceTheme.subtheme("dark");
+
+// for backwards-compatibility with apps that do not set their
+// own default theme:
+SC.defaultTheme = 'ace';
+
+/* >>>>>>>>>> BEGIN source/resources/button/dark/button.js */
+sc_require("theme");
+
+SC.AceTheme.Dark.PointLeft = SC.AceTheme.Dark.subtheme("point-left", "point-left");
+SC.AceTheme.Dark.PointRight = SC.AceTheme.Dark.subtheme("point-right", "point-right");
+SC.AceTheme.Dark.PointRight = SC.AceTheme.Dark.subtheme("capsule", "capsule");
+
+/* >>>>>>>>>> BEGIN source/resources/collection/source-list/source-list.js */
+SC.AceTheme.SourceList = SC.AceTheme.subtheme('source-list');
+
+// later we may define/override some constants here
+/* >>>>>>>>>> BEGIN source/resources/picker/ace/picker.js */
+
+/* >>>>>>>>>> BEGIN source/resources/picker/popover/popover.js */
+sc_require("theme");
+
+// it is derived from dark, but will be available both under Ace and not.
+SC.AceTheme.Popover = SC.AceTheme.Dark.subtheme("popover");
+SC.AceTheme.addTheme(SC.AceTheme.Popover);
+
+// there is a solid variety
+SC.AceTheme.SolidPopover = SC.AceTheme.Popover.subtheme('solid');
+
+// and a shortcut to the solid variety.
+SC.AceTheme.addTheme(SC.AceTheme.SolidPopover.create({ name: 'solid-popover' }));
+
+
+/* >>>>>>>>>> BEGIN source/resources/picker/popover/picker.js */
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2011 Strobe Inc. and contributors.
+//            Portions ©2008-2011 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+sc_require("resources/picker/popover/popover");
+
+SC.AceTheme.Popover.pickerRenderDelegate = SC.RenderDelegate.create({
+  name: 'picker',
+  
+  render: function(dataSource, context) {
+    var preferType = dataSource.get('preferType');
+    var pointerPosition = dataSource.get('pointerPos');
+    var pointerPositionY = dataSource.get('pointerPosY');
+
+    if (preferType == SC.PICKER_POINTER || preferType == SC.PICKER_MENU_POINTER) {
+      context.addClass(pointerPosition);
+    }
+  },
+  
+  update: function(dataSource, $) {
+    var preferType = dataSource.get('preferType');
+    var pointerPosition = dataSource.get('pointerPos');
+    var pointerPositionY = dataSource.get('pointerPosY');
+    
+    if (preferType == SC.PICKER_POINTER || preferType == SC.PICKER_MENU_POINTER) {
+      $.addClass(pointerPosition);
+    }
+    
+  }
+});
+/* >>>>>>>>>> BEGIN source/resources/picker/popover/workspace.js */
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2011 Strobe Inc. and contributors.
+//            Portions ©2008-2011 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+SC.AceTheme.Popover.workspaceRenderDelegate = SC.RenderDelegate.create({
+  name: 'workspace',
+  render: function(dataSource, context) {
+    context.setClass({
+      'top-toolbar': dataSource.get('hasTopToolbar'),
+      'bottom-toolbar': dataSource.get('hasBottomToolbar')
+    });
+    
+    context = context.begin('div').addClass('popover-background');
+    this.includeSlices(dataSource, context, SC.NINE_SLICE);
+    context.push("<div class = 'sc-pointer'></div>");
+    context = context.end();
+  },
+
+  update: function(dataSource, jquery) {
+    jquery.setClass({
+      'top-toolbar': dataSource.get('hasTopToolbar'),
+      'bottom-toolbar': dataSource.get('hasBottomToolbar')
+    });
+  }
+});
